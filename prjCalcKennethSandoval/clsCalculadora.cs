4602123baf3279ec26a24c6b2dbfc7191da3d73f
KreatorKen <kensandoval6@gmail.com>
2024-04-27 08:29:45 -0600

    first commit

---
+++ prjCalcKennethSandoval/clsCalculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjCalcKennethSandoval
+{
+    public class clsCalculadora
+    {
+        public static int Suma(int a, int b) {
+            return a + b;
+        }
+        public static int Resta(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplicacion(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
